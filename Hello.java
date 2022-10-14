a0848c38f9321e421581da5445cf28f87d4afcce
abinav macharam <abinav.may24@gmail.com>
2022-10-14 02:24:29 +0000

    changes

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
-{
-public static void main(args[])
-{
-system.print.out.ln("Hello")
-}
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
 }
